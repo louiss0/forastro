0e3fedb7defe3ad1e759588f1394d939df8ab9b4
Shelton Louis <louisshelton0@gmail.com>
2023-03-15 20:41:53 -0400

    Setup the scaffold for the utilities page

--- demo/tailwind.config.cjs
+++ demo/tailwind.config.cjs
@@ -1,10 +1,15 @@
-const { createGlobPatternsForDependencies } = require('@nxtensions/astro/tailwind');
-const { join } = require('path');
+const {
+  createGlobPatternsForDependencies,
+} = require("@nxtensions/astro/tailwind");
+const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,svelte,ts,tsx,vue}'),
+    join(
+      __dirname,
+      "src/**/!(*.stories|*.spec).{astro,html,js,jsx,md?x,svelte,ts,tsx,vue}"
+    ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
