53238c7fd64db8b523b12e7cf99b689c83525871
Shelton Louis <louisshelton0@gmail.com>
2023-03-16 20:59:44 -0400

    Finished scaffolding the page

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   outDir: "../dist/demo",
-  integrations: [tailwind(), mdx({})],
+  integrations: [tailwind()],
 });
