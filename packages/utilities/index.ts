713846d3b5674313e9c2df7cdf671187e65c9224
Shelton Louis <louisshelton0@gmail.com>
2023-04-24 17:10:02 -0400

    Finished creating both templates

--- packages/utilities/index.ts
+++ packages/utilities/index.ts
@@ -3,23 +3,22 @@ import Gap from "./src/lib/components/Gap.astro"
 import DefineTemplate from "./src/lib/components/DefineTemplate.astro"
 import Projector from "./src/lib/components/Projector.astro"
 import PageLink from "./src/lib/components/PageLink.astro"
-import IslandMirror from "./src/lib/components/IslandMIrror.astro"
 
 
 export {
     iterate,
-     range,
+    range,
     iterateRange,
     executeIf,
     executeUnless
- } from "./src/lib/helpers"
+} from "./src/lib/helpers"
 
 export type {
     HasForEachMethod,
     GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed,
-     IterationInfo,
-     IterateRangeCallback,
-     IterateRangeOptions
+    IterationInfo,
+    IterateRangeCallback,
+    IterateRangeOptions
 } from "./src/lib/types"
 
 export {
@@ -27,6 +26,5 @@ export {
     DefineTemplate,
     Projector,
     PageLink,
-    IslandMirror,
 }
 
--- packages/utilities/src/lib/helpers.ts
+++ packages/utilities/src/lib/helpers.ts
@@ -6,24 +6,23 @@ import {
     IterationInfo
 } from "./types"
 
-const templateMapSymbol = Symbol("template-map");
 
 const isIterable = (value: unknown): value is Iterable<unknown> =>
     isObject(value) && (
         typeof value[Symbol.iterator] === 'function'
         || typeof value[Symbol.asyncIterator] === 'function'
-    )    
+    )
 
-type Callback = (...args:Array<unknown>)=> any
+type Callback = (...args: Array<unknown>) => any
 
-function executeIf<T extends Callback>(condition:boolean, cb:T):ReturnType<T> | null  {
+function executeIf<T extends Callback>(condition: boolean, cb: T): ReturnType<T> | null {
 
 
     return condition ? cb() : null
 
 }
 
-function executeUnless<T extends Callback>(condition:boolean, cb:T) {
+function executeUnless<T extends Callback>(condition: boolean, cb: T) {
 
 
     return executeIf(!condition, cb)
@@ -31,27 +30,27 @@ function executeUnless<T extends Callback>(condition:boolean, cb:T) {
 }
 
 
-function isObject(value:unknown): value is Record<PropertyKey, unknown> {
+function isObject(value: unknown): value is Record<PropertyKey, unknown> {
 
     return typeof value === "object" && value != null
 }
 
- function hasForEachMethod(value:unknown): value is HasForEachMethod {
-	return  isObject(value) && 'forEach' in value
+function hasForEachMethod(value: unknown): value is HasForEachMethod {
+    return isObject(value) && 'forEach' in value
 }
 
 
 
-function* range(start:number,stop:number,step=1) {
-    
+function* range(start: number, stop: number, step = 1) {
+
 
     if (start === stop) {
-        
+
         throw new Error("Start can't be the same as stop")
     }
-    
+
     if (step === 0) {
-        
+
 
         throw new Error("Step can't be zero pick a negative or positive number")
 
@@ -64,55 +63,55 @@ function* range(start:number,stop:number,step=1) {
     const startIsGreaterThanStopAndStepIsPositive = start > stop && numIsPositive
 
     if (startIsGreaterThanStopAndStepIsPositive) {
-        
+
         throw new Error("If you want start to be greater than stop please make step negative")
 
     }
-    
+
     const stopIsGreaterThanStartAndStepIsNegative = start < stop && numIsNegative
 
     if (stopIsGreaterThanStartAndStepIsNegative) {
-        
+
         throw new Error("If you want start to be less than stop please make step positive")
 
     }
 
-    
 
-    let count = start 
+
+    let count = start
 
 
     if (numIsPositive) {
-        
+
 
         while (count < stop) {
-            
 
-            yield count 
+
+            yield count
 
             count += step
         }
 
         return
-    } 
- 
+    }
+
 
     if (numIsNegative) {
-        
-        while (count > stop) {
-            
 
-            yield count 
+        while (count > stop) {
+
+
+            yield count
 
             count += step
         }
 
     }
-    
-    
-    
-    
-    
+
+
+
+
+
 }
 
 
@@ -125,53 +124,53 @@ function isGenerator(value: unknown): value is Generator {
 
 
 
-function wrapFunctionInAsyncGenerator<T extends (...args:Array<any>)=> ReturnType<T>>(fn: T) {
+function wrapFunctionInAsyncGenerator<T extends (...args: Array<any>) => ReturnType<T>>(fn: T) {
 
-    return  async function* (...args:Parameters<T>) {
-        
+    return async function* (...args: Parameters<T>) {
+
         const res = fn(...args)
-        
+
         if (res instanceof Promise) {
             yield await res
-            
+
             return
         }
 
         yield res
-    
+
     }
 }
 
 
 async function* iterate<T extends Generator | HasForEachMethod, U>(
-    iterable: T, cb:GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed<T, U>) {
-    
+    iterable: T, cb: GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed<T, U>) {
+
 
     if (!isIterable(iterable)) {
-        
+
         throw new Error("You did not pass in an iterable")
     }
 
-    
-    
+
+
     if (hasForEachMethod(iterable)) {
-       
+
         let iteration = 0
 
         const convertedIterable = Object.entries(iterable)
-        for await (const [key, value] of  convertedIterable) {
-            
-            const numberFromParseIntOrStringKey = Number.isNaN(key)? key: parseInt(key)
-            iteration = iterable instanceof Array ?  parseInt(key) : iteration + 1  
-            
+        for await (const [key, value] of convertedIterable) {
+
+            const numberFromParseIntOrStringKey = Number.isNaN(key) ? key : parseInt(key)
+            iteration = iterable instanceof Array ? parseInt(key) : iteration + 1
+
             yield* wrapFunctionInAsyncGenerator(
                 cb as GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed<HasForEachMethod, U>)
                 (
-                value,
-                new IterationInfo(0, iteration, convertedIterable.length),
-                numberFromParseIntOrStringKey
+                    value,
+                    new IterationInfo(0, iteration, convertedIterable.length),
+                    numberFromParseIntOrStringKey
                 )
-            
+
         }
 
 
@@ -183,7 +182,7 @@ async function* iterate<T extends Generator | HasForEachMethod, U>(
 
 
     if (isGenerator(iterable)) {
-        
+
         for (const value of iterable) {
 
             yield* wrapFunctionInAsyncGenerator(cb as GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed<Generator, U>)(value)
@@ -191,64 +190,82 @@ async function* iterate<T extends Generator | HasForEachMethod, U>(
 
     }
 
-   
-    
 
-}
-
-type AstroRenderSlotFunction = (slotName: string, args: Array<any>) => Promise<string>
-    
-type TemplateMapObject<T extends Lowercase<string> = Lowercase<string> > = {[templateMapSymbol]: Map<T, (...args:Array<any>)=>ReturnType<AstroRenderSlotFunction>> }
-
-let globalTemplateMap: typeof globalThis & TemplateMapObject
-    
-function defineGlobalTemplateMap() {
-    
-
-    if(globalTemplateMap) { return }
-
-   globalTemplateMap = Object.assign(globalThis, {
-        [templateMapSymbol]: new Map(),
-    })
-
-    return globalTemplateMap;
-    
-    
-}
-
-function setToGlobalTemplateMap<T extends Lowercase<string> >(templateName:T, astroRenderFunction:(...args:Array<any>)=>ReturnType<AstroRenderSlotFunction>) {
-
-
-    return globalTemplateMap && globalTemplateMap[templateMapSymbol].set(templateName, astroRenderFunction)
 
 
 }
 
-function getFromGlobalTemplateMap<T extends Lowercase<string> >(templateName:T) {
+async function* iterateRange<T>(callback: IterateRangeCallback<T>, options: IterateRangeOptions) {
 
-    return globalTemplateMap && globalTemplateMap[templateMapSymbol].get(templateName)
-    
-}
-
-
-async function* iterateRange<T>(callback:IterateRangeCallback<T>, options:IterateRangeOptions) {
-    
     const { start, stop, step = 1 } = options
 
-    yield* iterate(range(start, stop, step), (val)=> callback(val as number, new IterationInfo(start, val as number, stop)) )
+    yield* iterate(range(start, stop, step), (val) => callback(val as number, new IterationInfo(start, val as number, stop)))
 
 }
+
+const templateMapSymbol = Symbol("template-map");
+
+
+type AstroRenderSlotFunction<U extends Array<any>> = <T extends Lowercase<string>>(slotName: T, args: U) => Promise<string>
+
+
+type ContextCurryRenderSlotFunction = <U extends Array<any>>(...args: U) => ReturnType<AstroRenderSlotFunction<U>>
+
+
+type TemplateMapObject = {
+    [templateMapSymbol]: Map<Lowercase<string>, ContextCurryRenderSlotFunction>
+}
+
+
+
+
+function defineGlobalTemplateMap() {
+
+
+
+    if (templateMapSymbol in globalThis) { return };
+
+
+    Object.assign(globalThis, {
+        [templateMapSymbol]: new Map<Lowercase<string>, ContextCurryRenderSlotFunction>(),
+    })
+}
+
+
+
+function getGlobalTemplateMap() {
+
+    return (globalThis as unknown as TemplateMapObject)[templateMapSymbol]
+
+}
+
+function setToGlobalTemplateMap(templateName: Lowercase<string>, astroRenderFunction: ContextCurryRenderSlotFunction) {
+
+
+    return getGlobalTemplateMap().set(templateName, astroRenderFunction)
+
+
+}
+
+function getFromGlobalTemplateMap(templateName: Lowercase<string>) {
+
+    return getGlobalTemplateMap().get(templateName)
+
+}
+
+
+
 
 
 
 export {
     range,
-     iterate,
-     iterateRange,
-     isIterable,
-     executeIf,
-     executeUnless,
-     defineGlobalTemplateMap,
-     setToGlobalTemplateMap,
-     getFromGlobalTemplateMap
+    iterate,
+    iterateRange,
+    isIterable,
+    executeIf,
+    executeUnless,
+    defineGlobalTemplateMap,
+    setToGlobalTemplateMap,
+    getFromGlobalTemplateMap
 }
