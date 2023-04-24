f6889cce1860956d5b83c1da80286a5eeeeb299e
Shelton Louis <louisshelton0@gmail.com>
2023-04-24 18:53:23 -0400

    Changed the lockfile

--- packages/templates/astro-vue-template/src/components/Card.vue
+++ packages/templates/astro-vue-template/src/components/Card.vue
@@ -1,4 +1,5 @@
----
+
+<script setup lang="ts" >
 import { getImage } from "astro:assets";
 interface Props {
   title: string;
@@ -6,19 +7,25 @@ interface Props {
   alt?: string;
 }
 
-const { title, src, alt } = Astro.props;
+const { title, src, alt } = defineProps<Props>();
 
 const imageMetadata = await getImage({ src });
+
+</script>
 ---
 
-<div class="grid">
+<template>
+  
+  <div class="grid">
   <header>
-    <img {...imageMetadata.attributes} alt={alt} />
+    <img v-bind="{...imageMetadata.attributes}" :alt="alt" />
   </header>
   <div class="py-2 px-4">
-    <div class="text-xl">{title}</div>
+    <div class="text-xl">{{title}}</div>
   </div>
   <footer class="">
     <slot />
   </footer>
 </div>
+
+</template>
