4d6b9f2eb1d8cc2e18c455425e251f7f2edd8a52
Shelton Louis <louisshelton0@gmail.com>
2023-04-24 17:10:02 -0400

    Finished creating both templates

--- packages/templates/astro-vue-template/astro.config.mjs
+++ packages/templates/astro-vue-template/astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver';
+import VueComponents from 'unplugin-vue-components/vite'
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      Icons(),
+      VueComponents({
+        resolvers: [
+          IconsResolver({})
+        ]
+      })
+    ],
+  },
   integrations: [
     tailwind(),
     vue({
+
       jsx: {
         enableObjectSlots: true,
       },
