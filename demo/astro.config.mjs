4c55efb1520ab3490d1653ee4e4eb37042180315
Shelton Louis <louisshelton0@gmail.com>
2023-04-26 11:59:31 -0400

    Changed the Read Me's for both astro minimal and astro vue template

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import mdx from "@astrojs/mdx";
+import remarkDirective from 'remark-directive';
+import remarkHtmlDirectives from 'remark-html-directives';
 export default defineConfig({
   outDir: "../dist/demo",
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx({
+    remarkPlugins: [
+      remarkDirective,
+      remarkHtmlDirectives
+    ]
+  })],
 });
