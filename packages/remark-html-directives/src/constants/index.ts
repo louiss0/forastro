6430a3822e7da26328cab9f3428b48e5184fa8d0
louiss0 <louisshelton0gmail.com>
2023-05-23 15:06:42 -0400

    Pushing the constants file

---
+++ packages/remark-html-directives/src/constants/index.ts
@@ -0,0 +1,99 @@
+const nodeDirectiveTypes = [
+    'textDirective',
+    'leafDirective',
+    'containerDirective',
+] as const;
+
+const supportedBlockLevelTags = [
+    'address',
+    'code',
+    'article',
+    'aside',
+    'blockquote',
+    "details",
+    "summary",
+    'div',
+    'dl',
+    'picture',
+    'figcaption',
+    'figure',
+    'footer',
+    'header',
+    'hr',
+    'li',
+    'main',
+    'nav',
+    'ol',
+    'p',
+    'pre',
+    'section',
+    'ul',
+    'video',
+    'audio',
+    "hgroup",
+    'table',
+    'tfoot',
+    'thead',
+    'tbody',
+] as const;
+
+const supportedInlineLevelTags = [
+    'br',
+    'button',
+    'i',
+    'img',
+    'map',
+    'iframe',
+    "source",
+    'span',
+] as const;
+
+const supportedTableTags = [
+    "tr",
+    "th",
+    "td",
+    "col",
+    "caption",
+    "colgroup",
+] as const
+
+const headings = [
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+] as const
+
+const supportedTextBasedTags = [
+    'cite',
+    'code',
+    'dfn',
+    'em',
+    'strong',
+    'sub',
+    'sup',
+    'time',
+    'var',
+    'mark',
+    'q',
+    'small',
+    'kbd',
+    'samp',
+    'a',
+    'abbr',
+    'bdo',
+    "data",
+    'dd',
+    'dt',
+] as const;
+
+export {
+    supportedBlockLevelTags,
+    supportedInlineLevelTags,
+    supportedTableTags,
+    headings,
+    nodeDirectiveTypes,
+    supportedTextBasedTags
+}
