23235352c50e4895709a96e545a103c3d82e90e8
louiss0 <louisshelton0gmail.com>
2023-05-23 22:21:49 -0400

    I fixed the types for remark and decided to update remark to 2.0.0

--- packages/remark-html-directives/src/types/index.ts
+++ packages/remark-html-directives/src/types/index.ts
@@ -40,7 +40,6 @@ type TextTags = typeof supportedTextBasedTags[number]
 type RareNodeTypes = "root" | "paragraph" | "listItem" | "thematicBreak" | "text"
 
 type NodeDirectiveObject = Node & {
-type NodeDirectiveObject = Node & {
   type: NodeDirectiveTypes | RareNodeTypes
   children: Array<Node>
   name: BlockElementNames | InlineElementNames | TableTagNames | TextTags
