1766d16c7745ced94329d02a612ca30abfea6dec
louiss0 <louisshelton0gmail.com>
2023-05-25 17:13:21 -0400

    Added the cunstom elements feature

--- packages/remark-html-directives/src/index.ts
+++ packages/remark-html-directives/src/index.ts
@@ -1,9 +1,13 @@
 
 import { Visitor, visit } from 'unist-util-visit'
 import { h } from 'hastscript'
-import { nodeDirectiveTypes } from 'src/constants'
+import { HTML_DIRECTIVE_MODES, nodeDirectiveTypes } from 'src/constants'
 import { NodeDirectiveObject, RemarkHTMLDirectivesConfig } from 'src/types'
-import { throwErrorIfANodeIsNotAViableNodeForArticles, throwErrorIfANodeIsNotAViableNodeForPages } from 'src/utils'
+import {
+  throwErrorIfANodeIsNotAViableNodeForArticles,
+  throwErrorIfANodeIsNotAViableNodeForPages,
+  overrideNodeDirectiveAttributesWithClassesAppendedToEachOtherAndTheRestOverWritten
+} from 'src/utils'
 
 
 
@@ -12,7 +16,7 @@ import { throwErrorIfANodeIsNotAViableNodeForArticles, throwErrorIfANodeIsNotAVi
 
 
 
-export default function HTMLDirectives(config: Partial<RemarkHTMLDirectivesConfig> = { mode: "article" }) {
+export default function HTMLDirectives(config: Partial<RemarkHTMLDirectivesConfig> = { mode: HTML_DIRECTIVE_MODES.ARTICLE }) {
 
   return (tree: Parameters<Visitor>[0]) => {
 
@@ -29,25 +33,67 @@ export default function HTMLDirectives(config: Partial<RemarkHTMLDirectivesConfi
 
 
       if (config.mode === "page") {
+  
+        
+        
+        if (config?.elements && nodeDirectiveObject.name in config.elements) {
+          
+          if (nodeDirectiveObject.type !== "containerDirective") {
+            
+
+            throw new Error(`
+              The node with this ${nodeDirectiveObject.name} must be a containerDirective.
+              in line ${nodeDirectiveObject.position ?? ""}   
+            `)
+
+          }
+
+          overrideNodeDirectiveAttributesWithClassesAppendedToEachOtherAndTheRestOverWritten(nodeDirectiveObject, config.elements)
+          
+           
+          Object.assign(nodeDirectiveObject.attributes, { dataElement: nodeDirectiveObject.name, dataForAstroRemarkDirective:true })
+          
+
+          nodeDirectiveObject.name = "div"
+          
+        }
+
+
         throwErrorIfANodeIsNotAViableNodeForPages(nodeDirectiveObject)
 
       }
 
+      
       if (config.mode === "article") {
+     
+        
         throwErrorIfANodeIsNotAViableNodeForArticles(nodeDirectiveObject)
+        
 
+        if (config?.elements && nodeDirectiveObject.name in config.elements) {
+  
+
+          overrideNodeDirectiveAttributesWithClassesAppendedToEachOtherAndTheRestOverWritten(nodeDirectiveObject, config.elements)
+
+          
+          Object.assign(nodeDirectiveObject.attributes, { dataForAstroRemarkDirective: true })
+
+  
+  
+        }
+        
+      
       }
 
 
-      if (config?.elements && nodeDirectiveObject.name in config.elements) {
 
-      }
 
       const data = nodeDirectiveObject.data || (nodeDirectiveObject.data = {})
 
 
       const hast = h(nodeDirectiveObject.name, nodeDirectiveObject.attributes)
 
+      
       data.hName = hast.tagName
 
       data.hProperties = hast.properties
--- packages/remark-html-directives/tsup.config.ts
+++ packages/remark-html-directives/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig((ctx)=> ({
   entry: ['src/index.ts'],
   splitting: false,
-  format: !ctx.watch ? ["cjs", "esm"] : "cjs",  
+  format: !ctx.watch ? ["cjs", "esm"] : "cjs", 
   dts: !ctx.watch,
   minify: !ctx.watch,
   clean: !ctx.watch,
