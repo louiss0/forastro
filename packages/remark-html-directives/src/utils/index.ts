3874b0fd5f16832b375bfa4d9054af8d4d944bea
louiss0 <louisshelton0gmail.com>
2023-05-25 17:13:21 -0400

    Added the cunstom elements feature

--- packages/remark-html-directives/src/utils/index.ts
+++ packages/remark-html-directives/src/utils/index.ts
@@ -1,9 +1,10 @@
-import { NodeDirectiveObject } from 'src/types';
+import { NodeDirectiveObject, RemarkHTMLDirectivesConfig } from 'src/types';
 import {
   headings,
   supportedBlockLevelTags,
   supportedInlineLevelTags,
-  supportedTableTags,
+  supportedBlockTableTags,
+  supportedInlineTableTags,
   supportedTextBasedTags
 } from 'src/constants';
 
@@ -13,7 +14,7 @@ import {
 
 const checkIfNodeTypeIsAViableContainerDirective = (node: NodeDirectiveObject) => [
   node.type === "containerDirective" && supportedBlockLevelTags.includes(node.name as any),
-  node.type === "containerDirective" && supportedTableTags.includes(node.name as any),
+  node.type === "containerDirective" && supportedBlockTableTags.includes(node.name as any),
 ].some((value) => value)
 
 
@@ -22,7 +23,7 @@ const checkIfNodeTypeIsAViableLeafDirective = (node: NodeDirectiveObject) => [
   node.type === "leafDirective" && supportedInlineLevelTags.includes(node.name as any),
   node.type === "leafDirective" && headings.includes(node.name as any),
   node.type === "leafDirective" && supportedTextBasedTags.includes(node.name as any),
-  node.type === "leafDirective" && supportedTableTags.includes(node.name as any),
+  node.type === "leafDirective" && supportedInlineTableTags.includes(node.name as any),
 ].some((value) => value)
 
 
@@ -48,13 +49,13 @@ function throwErrorIfANodeIsNotAViableNodeForPages(node: NodeDirectiveObject) {
     throw Error(`
 
     A container directive must be written using these tag names 
-    ${supportedBlockLevelTags.join(' , ')}${supportedTableTags.join(' , ')}.
+    ${supportedBlockLevelTags.join(' , ')}${supportedBlockTableTags.join(' , ')}.
     
     Remember to use ::: a name then put ::: at the bottom for them  
 
     A leaf directive must use these tag names 
     ${supportedInlineLevelTags.join(',')}${headings.join(",")}
-    ${supportedTextBasedTags.join(' , ')}${supportedTableTags.join(' , ')}
+    ${supportedTextBasedTags.join(' , ')}${supportedInlineLevelTags.join(' , ')}
 
     Remember to use :: for them 
 
@@ -73,6 +74,7 @@ function throwErrorIfANodeIsNotAViableNodeForArticles(node: NodeDirectiveObject)
 
 
   const directiveIsViable = [
+    supportedBlockTableTags.includes(node.name as any),
     checkIfNodeTypeIsAViableLeafDirective(node),
     checkIfNodeTypeIsAViableTextDirective(node),
   ].some((value) => value)
@@ -84,7 +86,7 @@ function throwErrorIfANodeIsNotAViableNodeForArticles(node: NodeDirectiveObject)
 
     A leaf directive must use these tag names 
     ${supportedInlineLevelTags.join(',')}${headings.join(",")}
-    ${supportedTextBasedTags.join(' , ')}${supportedTableTags.join(' , ')}
+    ${supportedTextBasedTags.join(' , ')}${supportedInlineLevelTags.join(' , ')}
 
     Remember to use :: for them 
 
@@ -99,7 +101,46 @@ function throwErrorIfANodeIsNotAViableNodeForArticles(node: NodeDirectiveObject)
 
 }
 
+  function overrideNodeDirectiveAttributesWithClassesAppendedToEachOtherAndTheRestOverWritten(
+        nodeDirectiveObject: NodeDirectiveObject,
+        elementAttrs: Exclude<RemarkHTMLDirectivesConfig["elements"], undefined>
+        ) {
+          
+        const nodeDirectiveObjectEntries = Object.entries(nodeDirectiveObject.attributes)
+  
+        const elementWithAttrs = elementAttrs[nodeDirectiveObject.name as keyof typeof elementAttrs]
+        
+      Object.assign(
+        nodeDirectiveObject.attributes,
+        nodeDirectiveObjectEntries.reduce(
+          generateObjectWithAppendedClassAndOverriddenAttrs(),
+          elementWithAttrs
+        )
+      ) 
+          
+
+      function generateObjectWithAppendedClassAndOverriddenAttrs() {
+        
+        return (carry: typeof elementWithAttrs, item: [string, string]) => {
+    
+          if (!carry)
+            return {};
+    
+          const [key, value] = item;
+    
+          return key === "class" ? { [key]: `${value} ${carry[key]}` } : Object.assign(carry, { [key]: value });
+    
+    
+    
+        };
+      }
+
+
+  }
+
+
 export {
   throwErrorIfANodeIsNotAViableNodeForArticles,
   throwErrorIfANodeIsNotAViableNodeForPages,
+  overrideNodeDirectiveAttributesWithClassesAppendedToEachOtherAndTheRestOverWritten
 }
