d3c05d10a0ab6d26eade76206b66a9145806e9cb
louiss0 <louisshelton0gmail.com>
2023-05-25 12:21:02 -0400

    Got rid of vite-plugin-dts in favor of tsup and set it up

---
+++ packages/remark-html-directives/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((ctx)=> ({
+  entry: ['src/index.ts'],
+  splitting: false,
+  format: !ctx.watch ? ["cjs", "esm"] : "cjs",  
+  dts: !ctx.watch,
+  minify: !ctx.watch,
+  clean: !ctx.watch,
+}))
