0bfa5a33cd7df22c398b5a8fcb1afd237f3742fd
louiss0 <louisshelton0gmail.com>
2023-05-27 11:58:30 -0400

    Using the forastro remark HTML directives integration in astro mdx

--- templates/astro-mdx/astro.config.mjs
+++ templates/astro-mdx/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import remarkHTMLDirectivesIntegration from "@forastro/remark-html-directives-integration";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), remarkHTMLDirectivesIntegration()],
 });
