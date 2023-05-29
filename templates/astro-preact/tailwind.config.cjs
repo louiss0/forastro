347dc0b96bea0eb4e8506f9fd14a5c6bd982781f
louiss0 <louisshelton0gmail.com>
2023-05-28 20:46:55 -0400

    Fixed the tailwind config for both the and the preact and mdx templates

--- templates/astro-preact/tailwind.config.cjs
+++ templates/astro-preact/tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    "hidden",
+    "fixed",
+    "translate-x-full",
+    "translate-y-full",
+    "opacity-0",
+  ],
+  plugins: [],
+};
