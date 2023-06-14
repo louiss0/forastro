8c1d12718617edc8bd45fc1dd956a2baabfbb86c
louiss0 <louisshelton0gmail.com>
2023-06-13 20:41:17 -0400

    Allowed global attributes to be exported.

--- packages/markdoc-html-tags/src/lib/index.ts
+++ packages/markdoc-html-tags/src/lib/index.ts
@@ -1,4 +1,17 @@
 export * from "./schema"
+export {
+    data as dataAttribute,
+    ariaHidden,
+    ariaLabel,
+    ariaLabelledBy,
+    contenteditable,
+    draggable,
+    spellcheck,
+    dir,
+    title,
+    translate,
+    lang,
+} from "./attributes"
 
 
 
