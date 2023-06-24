33b862db1e8d58f17641a0da1e28eb3555e8b1ac
louiss0 <louisshelton0gmail.com>
2023-06-24 18:09:00 -0400

    Moved the components to public so that they could be bundled.

---
+++ packages/flow/public/index.ts
@@ -0,0 +1,8 @@
+export { default as Case } from "./components/Case.astro";
+export { default as Switch } from "./components/Switch.astro";
+export { default as For } from "./components/For.astro";
+export { default as Show } from "./components/Show.astro";
+export { default as Hide } from "./components/Hide.astro";
+export { default as Range } from "./components/Range.astro";
+
+
