ef51a0490105a1df2f886e4eda918ec81817a809
louiss0 <louisshelton0gmail.com>
2023-06-24 19:02:58 -0400

    Moved the index file and removed relevant component imports from it as well.

---
+++ packages/utilities/src/index.ts
@@ -0,0 +1,21 @@
+
+
+
+export {
+    iterate,
+    syncIterate,
+    range,
+    iterateRange,
+    executeIf,
+    executeUnless
+} from "./lib/helpers"
+
+export type {
+    HasForEachMethod,
+    GetAppropriateFunctionBasedOnWhetherOrNotAGeneratorOfAnIterableWithTheForEachMethodIsPassed,
+    IterationInfo,
+    IterateRangeCallback,
+    IterateRangeOptions
+} from "./lib/types"
+
+
