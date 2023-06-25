c0ca6d4712852754d2ab8cee4fbc6a471d79b875
louiss0 <louisshelton0gmail.com>
2023-06-25 14:10:12 -0400

    Changed remark-html-directives to support only esm

--- packages/remark-html-directives/tsup.config.ts
+++ packages/remark-html-directives/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig((ctx) => ({
   entry: ['src/index.ts'],
   splitting: false,
-  format: ["cjs", "esm"],
+  format: "esm",
   dts: true,
   minify: !ctx.watch,
   clean: !ctx.watch,
