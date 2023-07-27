13753c79d0ac2238553e4f5d49e9bb86db82dcd2
louiss0 <louisshelton0gmail.com>
2023-07-26 20:13:13 -0400

    I'm finished with creating the template

--- templates/astro-markdoc/markdoc.config.mjs
+++ templates/astro-markdoc/markdoc.config.mjs
@@ -1,48 +1,11 @@
 import {
-  ul,
-  li,
-  abbr, 
-  a, 
-  figure, 
-  figcaption, 
-  details, 
-  summary,
-  dl,
-  dd,
-  dt,
-  del,
-  iframe,
-  video,
-  audio,
-  img
-} from '@forastro/markdoc-html-tags';
+  markdocHTMLTagSchemas
+} from 'markdoc-html-tag-schemas';
 
-import { defineMarkdocConfig, nodes } from '@astrojs/markdoc/config';
+import { defineMarkdocConfig,  } from '@astrojs/markdoc/config';
 
 export default defineMarkdocConfig({
-  nodes: {
-    
-    document: {
-      ...nodes.document,
-      render: null,
-    },
-  },
-  tags: {
-    ul,
-    li,
-    abbr,
-    iframe,
-    video,
-    audio,
-    del,
-    img,
-    a,
-    figure,
-    figcaption,
-    details,
-    summary,
-    dl,
-    dd,
-    dt
-  },
+  extends: [
+    markdocHTMLTagSchemas()
+  ]
 });
