dc047343cc5d251a7e04a9107b79e2f4e8c9d7e7
louiss0 <louisshelton0gmail.com>
2023-08-16 22:29:11 -0400

    chore(utilities): finish organizing the library

--- packages/utilities/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Gap } from "./Gap.astro"
-export { default as DefineTemplate } from "./DefineTemplate.astro"
-export { default as Projector } from "./Projector.astro"
-export { default as PageLink } from "./PageLink.astro"
--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -2,3 +2,12 @@ export { default as Gap } from "./lib/Gap.astro"
 export { default as DefineTemplate } from "./lib/DefineTemplate.astro"
 export { default as Projector } from "./lib/Projector.astro"
 export { default as PageLink } from "./lib/PageLink.astro"
+
+export {
+    executeIf,
+    executeUnless,
+    syncIterate,
+    iterate,
+    iterateRange,
+    range,
+} from "./lib/helpers"
