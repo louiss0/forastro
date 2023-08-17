5c0ff8424a291b393f7b36e2e479b342f1ccdd36
louiss0 <louisshelton0gmail.com>
2023-08-16 21:45:29 -0400

    chore: Remove  tsup.config files from flow and utilities.

--- packages/flow/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig((ctx) => ({
-  entry: ["src/helpers.ts"],
-  publicDir: true,
-  format: "esm",
-  minify: !ctx.watch,
-  clean: !ctx.watch,
-}))
