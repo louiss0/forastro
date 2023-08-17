377315ba4412b35ffca36f3d343804bf4849b046
louiss0 <louisshelton0gmail.com>
2023-08-16 22:17:41 -0400

    chore(utilities): create entry file for utilities

---
+++ packages/utilities/src/index.ts
@@ -0,0 +1,4 @@
+export { default as Gap } from "./lib/Gap.astro"
+export { default as DefineTemplate } from "./lib/DefineTemplate.astro"
+export { default as Projector } from "./lib/Projector.astro"
+export { default as PageLink } from "./lib/PageLink.astro"
