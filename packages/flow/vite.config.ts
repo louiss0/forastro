d6f7d44c6e1c943f0e0d71073295c0c2ad1dff02
louiss0 <louisshelton0gmail.com>
2023-08-16 22:37:27 -0400

    fix(flow&utilities): using the test folder instead of src

--- packages/flow/vite.config.ts
+++ packages/flow/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       dir: '../../node_modules/.vitest'
     },
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
   },
 });
