deb9c7be087b6b40cc452f00fd9a64bbc9fc347b
louiss0 <louisshelton0gmail.com>
2023-10-09 20:44:37 -0400

    remove remark-html-directives from mdx

--- templates/astro-mdx/astro.config.mjs
+++ templates/astro-mdx/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import forRemarkHtmlDirectivesIntegration from "@forastro/remark-html-directives-integration";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx({ extendMarkdownConfig: true }), forRemarkHtmlDirectivesIntegration()],
+  integrations: [
+    tailwind(),
+    mdx({ extendMarkdownConfig: true, }),
+  ],
 });
