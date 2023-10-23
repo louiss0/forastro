273b38c6d32e227eedf1ec0a3bc223bd784f1e9d
louiss0 <louisshelton0gmail.com>
2023-10-23 13:12:22 -0400

    refactor: Change get to content.
    I thought using get for this page was simply just bad. It's about expanding multiple collection helpers. Not just one.

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -2,5 +2,5 @@
 export { useTemplaterAndProjector } from "./lib/useTemplaterAndProjector.js"
 export * from "./lib/helpers/conditional"
 export * from "./lib/types"
-export * from "./lib/helpers/get-collection"
+export * from "./lib/helpers/content-collection"
 export * from "./lib/helpers"
