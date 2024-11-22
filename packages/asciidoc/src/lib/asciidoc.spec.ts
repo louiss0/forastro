2ecb6205661df6fea73da907d514d501fdf5ef6f
louiss0 <louisshelton0@gmail.com>
2024-11-22 13:01:31 -0500

    test: fast-glob library works and it can grab from a specific folder

--- packages/asciidoc/src/lib/asciidoc.spec.ts
+++ packages/asciidoc/src/lib/asciidoc.spec.ts
@@ -1,11 +1,30 @@
-import { asciidoc } from './asciidoc';
+import { glob } from "fast-glob";
 
 describe('asciidoc', () => {
-  it('should work', () => {
 
 
-    expect(asciidoc()).toEqual('asciidoc');
+  test("fast glob works", async () => {
 
 
-  });
+    const filePaths = await glob("**/*.adoc",)
+
+
+    expect(filePaths.length).toBeGreaterThan(0)
+
+
+  })
+
+
+  test("fast glob only grabs from a specific folder", async () => {
+
+
+    const filePaths = await glob("**/*.adoc", { cwd: import.meta.dirname })
+
+    expect(filePaths.length).toBeGreaterThan(0)
+
+    expect(filePaths).toContain("posts/page.adoc")
+
+
+  })
+
 });
