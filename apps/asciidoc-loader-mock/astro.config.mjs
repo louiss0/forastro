743d09ab245e2e4b5f3ae410ef2bebe052af012f
louiss0 <louisshelton0@gmail.com>
2024-11-28 18:30:02 -0500

    chore: create mock app for asciidoc loader

---
+++ apps/asciidoc-loader-mock/astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from 'node:url';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../../dist/apps/asciidoc-loader-mock',
+  integrations: [
+    tailwind({
+      configFile: fileURLToPath(
+        new URL('./tailwind.config.cjs', import.meta.url),
+      ),
+    }),
+  ],
+});
