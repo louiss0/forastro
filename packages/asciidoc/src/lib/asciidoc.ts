a341462dd5d894fb2e438f71c59c4546ec4a4558
louiss0 <louisshelton0@gmail.com>
2024-12-01 16:23:33 -0500

    feat: create schemas for people to use for asciidoc I created a normaal post schema which extracts expected post values. Then I created and extended version of that schema. That one uses a zod enum that tries to find out what stage a post is in. That enum is exported and ready to use. Each schema enforces the camelCase convention

--- packages/asciidoc/src/lib/asciidoc.ts
+++ packages/asciidoc/src/lib/asciidoc.ts
@@ -1,6 +1,7 @@
 import asciidoctor from "asciidoctor";
 import { type Loader } from "astro/loaders";
 import { createForAstroRegistryAsciidocFromConfig, generateSlug, getAsciidocPaths, getLoadAsciidocConfig, } from "./internal";
+import { z } from "astro/zod";
 
 const processor = asciidoctor()
 
@@ -8,8 +9,71 @@ const SUPPORTED_ASCIIDOC_FILE_EXTENSIONS = [".adoc", ".asciidoc"]
 
 const fullFilePathRE = /(?<folder_path>.+\/)(?<filename>[\w\s\d-]+)(?<extension>\.[a-z]+)/
 
-export function createAsciidocLoader(config_folder_name: string, folder_name: string) {
+const authorSchema = z.string().regex(
+    /^(?:[A-Z][a-z]+\s)+[A-Z][a-z]+$/,
+    "An author should have at least a first and last name both capitalized"
+);
+export const asciidocBaseSchema = z.object({
+    doctitle: z.string().regex(
+        /[\w\s\d]+/,
+        "Please make sure your title is one with words letters and spaces"
+    ),
+    docdate: z.string().date(),
+    email: z.string().email().optional(),
+    localdate: z.string().date(),
+    author: authorSchema,
+    authors: authorSchema.array(),
+    authorinitials: z.string().regex(/[A-Z]+/),
+    firstname: z.string().regex(/[A-Z][a-z]+/),
+    middlename: z.string().regex(/[A-Z][a-z]+/),
+    lastname: z.string().regex(/[A-Z][a-z]+/),
+    description: z.string()
+        .min(35, "It's good for a description to at least be two sentnces")
+        .max(160, "A title needs to be at least 160 characters max")
+}).transform((parsedInput) => {
 
+    const {
+        doctitle,
+        docdate,
+        localdate,
+        authorinitials,
+        firstname,
+        lastname,
+        middlename,
+        ...rest
+    } = parsedInput
+
+    return {
+        title: doctitle,
+        authorInitials: authorinitials,
+        firstName: firstname,
+        middleName: middlename,
+        lastName: lastname,
+        createdAt: localdate,
+        updatedAt: docdate,
+        ...rest
+    }
+
+})
+
+export const ASCIIDOC_POST_STAGE = z.enum(
+    [
+        "draft",
+        "published",
+        "editing"
+    ]
+);
+
+export type PostStage = z.infer<typeof ASCIIDOC_POST_STAGE>
+
+export const asciidocDraftSchema = asciidocBaseSchema.and(
+    z.object({
+        stage: ASCIIDOC_POST_STAGE
+    })
+)
+
+
+export function createAsciidocLoader(config_folder_name: string, folder_name: string) {
 
     return {
         name: "forastro/asciidoc-loader",
