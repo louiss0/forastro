b9a057c89d2d868e3ccaa171a5e96bd5fb7e9b2f
louiss0 <louisshelton0@gmail.com>
2024-12-01 12:02:02 -0500

    refactor: remove the asciidoc integration The asciidoc integration can't work. What's rendered by the loader can't be picked up by vite The server can be reloaded by using the loader

--- apps/asciidoc-loader-mock/astro.config.mjs
+++ apps/asciidoc-loader-mock/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
-
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/asciidoc-loader-mock',
