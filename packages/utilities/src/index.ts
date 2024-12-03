50a91abfc73cf2275c24bb6f8b91dbf2cacf72d3
louiss0 <louisshelton0@gmail.com>
2024-12-03 08:49:56 -0500

    refactor: move files into temp repo I need to recreate the utilities and asciidoc projects as nx projects

--- packages/utilities/src/index.ts
+++
@@ -1,6 +0,0 @@
-
-export { useTemplaterAndProjector } from "./lib/useTemplaterAndProjector.js"
-export * from "./lib/helpers/conditional"
-export * from "./lib/types"
-export * from "./lib/helpers/content-collection"
-export * from "./lib/helpers/index"
