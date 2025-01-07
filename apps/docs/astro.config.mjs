fef3452826458ad3eb2febbd36fedfe1a393c09d
louiss0 <louisshelton0@gmail.com>
2025-01-07 07:15:17 -0500

    fix: add title to the starlight integration

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -3,5 +3,9 @@ import StarlightIntegration from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/docs',
-  integrations: [StarlightIntegration()],
+  integrations: [
+    StarlightIntegration({
+      title: 'For Astro',
+    }),
+  ],
 });
