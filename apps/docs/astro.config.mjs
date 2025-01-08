f17d41fd93a8970c7fc145ff0a1e4f4944174f6c
louiss0 <louisshelton0@gmail.com>
2025-01-07 21:56:57 -0500

    change: site title and github link

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -6,10 +6,13 @@ import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight',
-    },
-  }), markdoc()],
+  integrations: [
+    starlight({
+      title: 'For Astro',
+      social: {
+        github: 'https://github.com/louiss0/forastro',
+      },
+    }),
+    markdoc(),
+  ],
 });
