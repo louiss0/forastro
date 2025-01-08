115c38620ebef9896dc203b2565b7aecb90c0fd9
louiss0 <louisshelton0@gmail.com>
2025-01-07 22:30:29 -0500

    fix: move config file to the root of the project

--- apps/docs/markdoc.config.mjs
+++ apps/docs/markdoc.config.mjs
@@ -1,6 +1,9 @@
 import { defineMarkdocConfig } from '@astrojs/markdoc/config';
 import starlightMarkdoc from '@astrojs/starlight-markdoc';
 
+const extension = starlightMarkdoc();
+
 export default defineMarkdocConfig({
-  extends: [starlightMarkdoc()],
+  ...extension,
+  extends: [],
 });
